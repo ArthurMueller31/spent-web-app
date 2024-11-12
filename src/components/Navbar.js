a6d2d87aeca2f508b46a7edcb680c159a4e8bc62
Arthur <arthurmueller31@gmail.com>
2024-11-12 16:44:23 -0300

    Working scrape

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="main-navbar">
-      <img className="spent-logo" src='../spent-logo-text-right.png' alt='app-logo' />
+      <img className="spent-logo" src='../public/spent-logo-text-right.png' alt='app-logo' />
       <Link to={"/sobre"}>Sobre</Link>
     </nav>
   )
