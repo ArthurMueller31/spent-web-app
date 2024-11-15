1fd06752b9ff541af3b782778c25fae6388a2b5e
Arthur <arthurmueller31@gmail.com>
2024-11-15 14:06:15 -0300

    Change post link

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -54,7 +54,7 @@ const MainHeader = () => {
   const returnData = async () => {
     setIsLoading(true);
     try {
-      const response = await axios.post("https://spent-webapp.up.railway.app/api/scrape", {
+      const response = await axios.post("https://spent-web-app.onrender.com/api/scrape", {
         url: url
       });
 
